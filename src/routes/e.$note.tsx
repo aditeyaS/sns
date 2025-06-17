c95c59426912bbdd8b5795c844f0d00356343a50
Aditeya Srivastava <this.is.aditeya@gmail.com>
2025-06-17 14:03:35 -0400

    removed unnecessary libraries

--- src/routes/e.$note.tsx
+++ src/routes/e.$note.tsx
@@ -1,47 +1,27 @@
+import { useState } from "react";
+import { toast } from "sonner";
+import { LockOpenIcon } from "lucide-react";
+import { createFileRoute } from "@tanstack/react-router";
+
 import { NoteViewCard } from "@/components/NoteViewCard";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { decrypt } from "@/utils/crypto";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createFileRoute } from "@tanstack/react-router";
-import { LockOpenIcon } from "lucide-react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import z from "zod";
 
 export const Route = createFileRoute("/e/$note")({
   component: RouteComponent,
 });
 
-const formSchema = z.object({
-  phrase: z.string().min(1),
-});
-
 function RouteComponent() {
   const { note } = Route.useParams();
+  const [phrase, setPhrase] = useState<string>("");
   const [decryptedNote, setDecryptedNote] = useState<string>();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      phrase: "",
-    },
-  });
-
-  async function onDecrypt(values: z.infer<typeof formSchema>) {
+  async function handleDecrypt() {
     try {
-      const decrypted = await decrypt(note, values.phrase);
+      const decrypted = await decrypt(note, phrase);
       toast.success("🎉 Note decrypted");
       setDecryptedNote(decrypted);
     } catch (error: any) {
@@ -61,29 +41,17 @@ function RouteComponent() {
             <span className="mt-2 text-2xl font-bold">{note}</span>
           </CardContent>
           <CardFooter>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onDecrypt)}
-                className="flex flex-col gap-2 items-start w-full"
-              >
-                <FormField
-                  control={form.control}
-                  name="phrase"
-                  render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel>Encryption phrase</FormLabel>
-                      <FormControl>
-                        <Input placeholder="mugiwara" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit">
-                  <LockOpenIcon /> Decrypt
-                </Button>
-              </form>
-            </Form>
+            <div className="flex flex-col items-start space-y-2 w-full">
+              <Label>Encryption phrase *</Label>
+              <Input
+                placeholder="mugiwara"
+                value={phrase}
+                onChange={(e) => setPhrase(e.target.value)}
+              />
+              <Button onClick={handleDecrypt} disabled={phrase === ""}>
+                <LockOpenIcon /> Decrypt
+              </Button>
+            </div>
           </CardFooter>
         </Card>
       )}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,105 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import { NoteSaveCard } from "@/components/NoteSaveCard";
-import { NoteEditorCard } from "@/components/NoteEditorCard";
+import { useEffect, useState } from "react";
+import { compressToEncodedURIComponent } from "lz-string";
+import { encrypt } from "@/utils/crypto";
+import { toast } from "sonner";
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { CopyIcon } from "lucide-react";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
 function App() {
-  const [savedUrl, setSavedUrl] = useState<string>();
+  const [note, setNote] = useState<string>("");
+  const [phrase, setPhrase] = useState<string>("");
+  const [noteUrl, setNoteUrl] = useState<string>("");
+
+  useEffect(() => {
+    const baseUrl = window.location.origin;
+    const updateNoteUrl = async () => {
+      if (phrase === "") {
+        const compressed = compressToEncodedURIComponent(note);
+        setNoteUrl(`${baseUrl}/${compressed}`);
+      } else {
+        const encoded = await encrypt(note, phrase);
+        setNoteUrl(`${baseUrl}/e/${encoded}`);
+      }
+    };
+    updateNoteUrl();
+  }, [note, phrase]);
+
+  async function handleCopyToClipboard() {
+    if (!navigator.clipboard) {
+      toast.error("Clipboard API is not supported in this browser.");
+      return;
+    }
+    try {
+      await navigator.clipboard.writeText(noteUrl);
+      toast.success("Link is copied to clipboard");
+    } catch (error: any) {
+      console.error("Clipboard error:", error);
+      const message =
+        typeof error?.message === "string"
+          ? error.message
+          : "Failed to copy the link to clipboard.";
+      toast.error(message);
+    }
+  }
 
   return (
-    <>
-      {savedUrl ? (
-        <NoteSaveCard url={savedUrl} onNew={() => setSavedUrl(undefined)} />
-      ) : (
-        <NoteEditorCard
-          onSave={(url) => setSavedUrl(`${window.location}${url}`)}
-        />
+    <Card>
+      <CardContent className="space-y-6">
+        <div className="space-y-2">
+          <Label htmlFor="note">Note *</Label>
+          <Textarea
+            id="note"
+            name="note"
+            placeholder="Write your note here..."
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="phrase">Encryption phrase</Label>
+          <Input
+            id="phrase"
+            name="phrase"
+            placeholder="Write your note here..."
+            value={phrase}
+            onChange={(e) => setPhrase(e.target.value)}
+          />
+          <p className="text-xs text-muted-foreground">
+            You will need this phrase to decrypt your note. Leave it empty if
+            you don't want to encrypt your notes.
+          </p>
+        </div>
+      </CardContent>
+      {note !== "" && (
+        <CardContent className="flex flex-col items-start">
+          <Separator className="mb-6" />
+          <Label>Sharable link</Label>
+          <p className="text-xs text-muted-foreground my-1">
+            You can share this link for anyone to access:
+          </p>
+          <a
+            target="_blank"
+            href={noteUrl}
+            className="text-2xl underline-offset-4 hover:underline break-words whitespace-pre-wrap overflow-hidden"
+          >
+            {noteUrl}
+          </a>
+          <Button className="mt-2" onClick={handleCopyToClipboard}>
+            <CopyIcon /> Copy link
+          </Button>
+        </CardContent>
       )}
-    </>
+    </Card>
   );
 }
